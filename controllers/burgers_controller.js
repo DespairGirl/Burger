e747100e53dbd9d0ccca4a6f5cb8a4b26b9f805f
MiMi T <truttling.mariah@gmail.com>
2021-03-10 17:03:54 -0500

    updating files

---
+++ controllers/burgers_controller.js
@@ -0,0 +1,59 @@
+const express = require('express');
+
+const router = express.Router();
+
+// 
+const burger = require('../models/burger');
+
+
+router.get('/', (req, res) => {
+  burger.all((data) => {
+    const hbsObject = {
+      burgers: data,
+    };
+    console.log(hbsObject);
+    res.render('index', hbsObject);
+  });
+});
+
+router.post('/api/burgers', (req, res) => {
+  burger.create(['name', 'devour'], [req.body.name, req.body.sleepy], (result) => {
+    // Send back the ID of the new quote
+    res.json({ id: result.insertId });
+  });
+});
+
+router.put('/api/burgers/:id', (req, res) => {
+  const condition = `id = ${req.params.id}`;
+
+  console.log('condition', condition);
+
+  burger.update(
+    {
+      sleepy: req.body.sleepy,
+    },
+    condition,
+    (result) => {
+      if (result.changedRows === 0) {
+       
+        return res.status(404).end();
+      }
+      res.status(200).end();
+    }
+  );
+});
+
+router.delete('/api/burgers/:id', (req, res) => {
+  const condition = `id = ${req.params.id}`;
+
+  cat.delete(condition, (result) => {
+    if (result.affectedRows === 0) {
+      
+      return res.status(404).end();
+    }
+    res.status(200).end();
+  });
+});
+
+// Export routes for server.js to use.
+module.exports = router;
---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO burgers (name) VALUES ('Black Bean');
+INSERT INTO burgers (name) VALUES ('Impossible Burger');
+INSERT INTO burgers (name, devour) VALUES ('Double Cheese Burger', true);
+INSERT INTO burgers (name, devour) VALUES ('Plain Burger', true);
+INSERT INTO burgers (name, devour) VALUES ('Ice Cream Dorito Burger', true);
+INSERT INTO burgers (name) VALUES ('Chicken Burger');
+INSERT INTO burgers (name) VALUES ('Shrimp Scampic Burger');
