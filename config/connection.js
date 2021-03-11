171e7d061ad846b8d8cfba223c264f04708d8bfa
Ult Des <truttling.mariah@gmail.com>
2021-03-11 00:29:27 -0500

    changed connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,7 +1,12 @@
 // Set up MySQL connection.
 const mysql = require('mysql');
 
-const connection = mysql.createConnection({
+const connection;
+if(process.env.JAWSDB_URL){
+  mysql.createConnection(process.env.JAWSDB_URL)
+}
+else{
+mysql.createConnection({
   host: 'localhost',
   port: 3306,
   user: 'root',
@@ -9,6 +14,7 @@ const connection = mysql.createConnection({
   password: 'mimi2131!!',
   database: 'burgers_db',
 });
+};
 
 // Make connection.
 connection.connect((err) => {
